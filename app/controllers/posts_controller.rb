e5afa97878d5287330641d24278f091e8b09a08a
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-03 21:47:02 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,9 +11,10 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 	def create
-  post = Post.new
-  post.title = params[:title]
-  post.description = params[:description]
-  post.save
+  @post = Post.new
+  @post.title = params[:title]
+  @post.description = params[:description]
+  @post.save
+  redirect_to post_path(@post)
 end
 end
